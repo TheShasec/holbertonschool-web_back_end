9f372928ad73489a5dee889a833120e85eb216c6
theshasec <theshasec@gmail.com>
2025-11-29 15:13:14 +0000

    fsdf

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+fdsa
+asdf
+a
+sf
+assdadsf
+sasadsdsfdsfsf
+sa
+dfadfsdfafsfafasdfdfsdfasfd
+"""
+
+
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """sdafsdfasdfdaf
+    fdfadfsdaff
+    fasasfljdfjalsf
+    fsjldkfslfaj
+    fa
+    jdfklsfjlafjkdsl"""
+
+    return mongo_collection.insert_one({"name":kwargs["name"] , "address":kwargs["address"]}).inserted_id
