d319f88e30eaf271355a25a96b034ed51d03fc20
theshasec <theshasec@gmail.com>
2025-11-29 15:33:48 +0000

    fsdf

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python3
-"""
-fals;dfjlsdjfsdfjsldfjdskjf
-fsafjskdlfjksdjfksdfjdkf
-"""
+"""Script that provides some stats about Nginx logs stored in MongoDB"""
 
 from pymongo import MongoClient
 
 if __name__ == "__main__":
-    """ fsafjsaflksadjfdfkdfksdf
-        fksdjafodifjoewfieosd
-    """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
+    db = client.logs
+    nginx_collection = db.nginx
 
-    n_logs = nginx_collection.count_documents({})
-    print(f'{n_logs} logs')
+    # Total logs
+    total_logs = nginx_collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    # Methods stats
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print('Methods:')
+    print("Methods:")
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
-        print(f'\tmethod {method}: {count}')
+        print(f"\tmethod {method}: {count}")
 
-    status_check = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"}
-        )
-    print(f'{status_check} status check')
+    # Status check (GET /status)
+    status_check = nginx_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
+
