527d145249a863706fcefcff8c7f31634b86dde8
theshasec <theshasec@gmail.com>
2025-11-29 15:22:02 +0000

    fsdf

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""423fefewf
+sdfsdafsafas
+fsdfsafdsafsaf
+sdfsdfsdfsdf
+asdfsdfafasf
+dfafas
+fsfsd
+sddfssasfsf"""
+
+
+import pymongo 
+
+
+def update_topics(mongo_collection, name, topics):
+    """wrwefasfsdfsad
+    fasfsadfdsfsdfdsfdsd
+    fdsafdasfdfd
+    fdsafdfsdf34f34r34315
+    4
+    rfsfsdfdsdfds
+    3434"""
+
+    mongo_collection.update_many({"name":name}, {"topics":topics})
